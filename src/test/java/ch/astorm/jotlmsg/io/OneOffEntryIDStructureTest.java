e631145e1e07c74e25d132272f379737ebd62434
Guido Stein <guido.stein@stein-engineering.de>
2021-02-07 20:54:08 +0100

    Use try-with-resources.

--- src/test/java/ch/astorm/jotlmsg/io/OneOffEntryIDStructureTest.java
+++ src/test/java/ch/astorm/jotlmsg/io/OneOffEntryIDStructureTest.java
@@ -19,86 +19,82 @@ public class OneOffEntryIDStructureTest {
 
     @Test
     public void testOneOffEntryIDStructure1() throws IOException {
-        InputStream msg = OneOffEntryIDStructureTest.class.getResourceAsStream("../msoutlook/replyto.msg");
-        MAPIMessage mapiMessage = new MAPIMessage(msg);
-
-        byte[] msgBytes = null;
-        
-        List<Chunk> replyEntriesChunks = mapiMessage.getMainChunks().getAll().get(MAPIProperty.REPLY_RECIPIENT_ENTRIES);
-        if(replyEntriesChunks!=null && !replyEntriesChunks.isEmpty()) {
-            for(Chunk chunk : replyEntriesChunks) {
-                if(chunk instanceof ByteChunk) {
-                    ByteChunk bc = (ByteChunk)chunk;
-                    msgBytes = bc.getValue();
+        try(InputStream msg = OneOffEntryIDStructureTest.class.getResourceAsStream("../msoutlook/replyto.msg");
+            MAPIMessage mapiMessage = new MAPIMessage(msg)) {
+            
+            byte[] msgBytes = null;
+            
+            List<Chunk> replyEntriesChunks = mapiMessage.getMainChunks().getAll().get(MAPIProperty.REPLY_RECIPIENT_ENTRIES);
+            if(replyEntriesChunks!=null && !replyEntriesChunks.isEmpty()) {
+                for(Chunk chunk : replyEntriesChunks) {
+                    if(chunk instanceof ByteChunk) {
+                        ByteChunk bc = (ByteChunk)chunk;
+                        msgBytes = bc.getValue();
+                    }
                 }
             }
+            assertNotNull(msgBytes);
+            FlatEntryListStructure<OneOffEntryIDStructure> fels = new FlatEntryListStructure<>(OneOffEntryIDStructure.class, msgBytes);
+            
+            assertNotNull(fels);
+            assertEquals(2, fels.getCount());
+            assertEquals(208, fels.getSize());
+            
+            List<OneOffEntryIDStructure> ooels = fels.getFlatEntryStructures();
+            assertNotNull(ooels);
+            OneOffEntryIDStructure ooe1 = ooels.get(0);
+            assertEquals(98, ooe1.getSize());
+            assertEquals("reply1@test.com", ooe1.getDisplayName());
+            assertEquals("reply1@test.com", ooe1.getEmailAddress());
+            OneOffEntryIDStructure ooe2 = ooels.get(1);
+            assertEquals(98, ooe2.getSize());
+            assertEquals("reply2@test.com", ooe2.getDisplayName());
+            assertEquals("reply2@test.com", ooe2.getEmailAddress());
         }
-        assertNotNull(msgBytes);
-        FlatEntryListStructure<OneOffEntryIDStructure> fels = new FlatEntryListStructure<>(OneOffEntryIDStructure.class, msgBytes);
-        
-        assertNotNull(fels);
-        assertEquals(2, fels.getCount());
-        assertEquals(208, fels.getSize());
-        
-        List<OneOffEntryIDStructure> ooels = fels.getFlatEntryStructures();
-        assertNotNull(ooels);
-        OneOffEntryIDStructure ooe1 = ooels.get(0);
-        assertEquals(98, ooe1.getSize());
-        assertEquals("reply1@test.com", ooe1.getDisplayName());
-        assertEquals("reply1@test.com", ooe1.getEmailAddress());
-        OneOffEntryIDStructure ooe2 = ooels.get(1);
-        assertEquals(98, ooe2.getSize());
-        assertEquals("reply2@test.com", ooe2.getDisplayName());
-        assertEquals("reply2@test.com", ooe2.getEmailAddress());
-        
-        mapiMessage.close();
-        msg.close();
     }
 
     @Test
     public void testOneOffEntryIDStructure2() throws IOException {
-        InputStream msg = OneOffEntryIDStructureTest.class.getResourceAsStream("../msoutlook/replyto.msg");
-        MAPIMessage mapiMessage = new MAPIMessage(msg);
-        
-        byte[] msgBytes = null;
-        
-        List<Chunk> replyEntriesChunks = mapiMessage.getMainChunks().getAll().get(MAPIProperty.REPLY_RECIPIENT_ENTRIES);
-        if(replyEntriesChunks!=null && !replyEntriesChunks.isEmpty()) {
-            for(Chunk chunk : replyEntriesChunks) {
-                if(chunk instanceof ByteChunk) {
-                    ByteChunk bc = (ByteChunk)chunk;
-                    msgBytes = bc.getValue();
+        try(InputStream msg = OneOffEntryIDStructureTest.class.getResourceAsStream("../msoutlook/replyto.msg");
+            MAPIMessage mapiMessage = new MAPIMessage(msg)) {
+            
+            byte[] msgBytes = null;
+            
+            List<Chunk> replyEntriesChunks = mapiMessage.getMainChunks().getAll().get(MAPIProperty.REPLY_RECIPIENT_ENTRIES);
+            if(replyEntriesChunks!=null && !replyEntriesChunks.isEmpty()) {
+                for(Chunk chunk : replyEntriesChunks) {
+                    if(chunk instanceof ByteChunk) {
+                        ByteChunk bc = (ByteChunk)chunk;
+                        msgBytes = bc.getValue();
+                    }
                 }
             }
+            assertNotNull(msgBytes);
+            FlatEntryListStructure<OneOffEntryIDStructure> fels1 = new FlatEntryListStructure<>(OneOffEntryIDStructure.class, msgBytes);
+            
+            FlatEntryListStructure<OneOffEntryIDStructure> fels2 = new FlatEntryListStructure<>();
+            fels2.addFlatEntryStructure(new OneOffEntryIDStructure("reply1@test.com", "reply1@test.com"));
+            fels2.addFlatEntryStructure(new OneOffEntryIDStructure("reply2@test.com", "reply2@test.com"));
+            byte[] newBytes = fels2.toBytes();
+            // assertArrayEquals(msgBytes, newBytes); doesn't work: alignment bytes with random values in msg file!  
+            fels2 = new FlatEntryListStructure<OneOffEntryIDStructure>(OneOffEntryIDStructure.class, newBytes);
+            
+            assertNotNull(fels1);
+            assertNotNull(fels2);
+            assertEquals(fels1.getCount(), fels2.getCount());
+            assertEquals(fels1.getSize(), fels2.getSize());
+            
+            Iterator<OneOffEntryIDStructure> ooels1Iterator = fels1.iterator();
+            Iterator<OneOffEntryIDStructure> ooels2Iterator = fels2.iterator();
+            while(ooels1Iterator.hasNext() && ooels2Iterator.hasNext()) {
+                 OneOffEntryIDStructure ooe1 = ooels1Iterator.next();
+                 OneOffEntryIDStructure ooe2 = ooels2Iterator.next();
+                 assertEquals(ooe1.getSize(), ooe2.getSize());
+                 assertArrayEquals(ooe1.getEntryID(), ooe2.getEntryID());
+                 assertEquals(ooe1.getDisplayName(), ooe2.getDisplayName());
+                 assertEquals(ooe1.getEmailAddress(), ooe2.getEmailAddress());
+            }
         }
-        assertNotNull(msgBytes);
-        FlatEntryListStructure<OneOffEntryIDStructure> fels1 = new FlatEntryListStructure<>(OneOffEntryIDStructure.class, msgBytes);
-        
-        FlatEntryListStructure<OneOffEntryIDStructure> fels2 = new FlatEntryListStructure<>();
-        fels2.addFlatEntryStructure(new OneOffEntryIDStructure("reply1@test.com", "reply1@test.com"));
-        fels2.addFlatEntryStructure(new OneOffEntryIDStructure("reply2@test.com", "reply2@test.com"));
-        byte[] newBytes = fels2.toBytes();
-        // assertArrayEquals(msgBytes, newBytes); doesn't work: alignment bytes with random values in msg file!  
-        fels2 = new FlatEntryListStructure<OneOffEntryIDStructure>(OneOffEntryIDStructure.class, newBytes);
-        
-        assertNotNull(fels1);
-        assertNotNull(fels2);
-        assertEquals(fels1.getCount(), fels2.getCount());
-        assertEquals(fels1.getSize(), fels2.getSize());
-        
-        Iterator<OneOffEntryIDStructure> ooels1Iterator = fels1.iterator();
-        Iterator<OneOffEntryIDStructure> ooels2Iterator = fels2.iterator();
-        while(ooels1Iterator.hasNext() && ooels2Iterator.hasNext()) {
-             OneOffEntryIDStructure ooe1 = ooels1Iterator.next();
-             OneOffEntryIDStructure ooe2 = ooels2Iterator.next();
-             assertEquals(ooe1.getSize(), ooe2.getSize());
-             assertArrayEquals(ooe1.getEntryID(), ooe2.getEntryID());
-             assertEquals(ooe1.getDisplayName(), ooe2.getDisplayName());
-             assertEquals(ooe1.getEmailAddress(), ooe2.getEmailAddress());
-        }
-        
-        mapiMessage.close();
-        msg.close();
     }
     
     @Test
